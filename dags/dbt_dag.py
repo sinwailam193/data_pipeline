7909a27d58948208195c6d1c176cba0f88bcdf85
sinwailam193 <8454621+sinwailam193@users.noreply.github.com>
2025-11-29 18:39:20 +0800

    adding airflow dag

---
+++ dags/dbt_dag.py
@@ -0,0 +1,27 @@
+import os
+from datetime import datetime
+
+from cosmos import DbtDag, ProfileConfig, ProjectConfig, ExecutionConfig
+from cosmos.profiles import SnowflakePrivateKeyPemProfileMapping
+
+profile_config = ProfileConfig(
+    profile_name="default",
+    target_name="dev",
+    profile_mapping=SnowflakePrivateKeyPemProfileMapping(
+        conn_id="snowflake_conn",
+        profile_args={"database": "dbt_db", "schema": "dbt_schema"},
+    ),
+)
+
+dbt_snowflake_dag = DbtDag(
+    project_config=ProjectConfig("/usr/local/airflow/dags/dbt/data_pipeline"),
+    operator_args={"install_deps": True},
+    profile_config=profile_config,
+    execution_config=ExecutionConfig(
+        dbt_executable_path=f"{os.environ['AIRFLOW_HOME']}/dbt_venv/bin/dbt",
+    ),
+    schedule="@daily",
+    start_date=datetime(2023, 1, 1),
+    catchup=False,
+    dag_id="dbt_dag",
+)
